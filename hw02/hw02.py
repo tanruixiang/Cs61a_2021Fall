3bd66619b79a103734e7eea429748c96bb2d48be
tanruixiang <819464715@qq.com>
2021-12-22 11:20:36 +0800

    Pass hw02(without Church numerals)

--- hw02/hw02.py
+++ hw02/hw02.py
@@ -31,7 +31,10 @@ def product(n, term):
     >>> product(3, triple)    # 1*3 * 2*3 * 3*3
     162
     """
-    "*** YOUR CODE HERE ***"
+    sum=1
+    for i in range(1,n+1):
+        sum=sum*term(i)
+    return sum
 
 
 def square(x):
@@ -62,7 +65,9 @@ def accumulate(merger, base, n, term):
     >>> accumulate(lambda x, y: (x + y) % 17, 19, 20, square)
     16
     """
-    "*** YOUR CODE HERE ***"
+    for i in range(1,n+1):
+        base=merger(base,term(i))
+    return base
 
 
 def summation_using_accumulate(n, term):
@@ -73,7 +78,7 @@ def summation_using_accumulate(n, term):
     >>> summation_using_accumulate(5, triple)
     45
     """
-    "*** YOUR CODE HERE ***"
+    return accumulate(add,0,n,term)
 
 
 def product_using_accumulate(n, term):
@@ -84,7 +89,7 @@ def product_using_accumulate(n, term):
     >>> product_using_accumulate(6, triple)
     524880
     """
-    "*** YOUR CODE HERE ***"
+    return accumulate(mul,1,n,term)
 
 
 def accumulate_syntax_check():
