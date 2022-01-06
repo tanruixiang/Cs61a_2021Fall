f16973f1867796025a0a9797af80827783fc8a96
tanruixiang <819464715@qq.com>
2022-01-06 16:46:45 +0800

    Pass lab10

--- lab10/lab10.scm
+++ lab10/lab10.scm
@@ -1,9 +1,24 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2) 
+    (cond
+        ((> num1 num2) 1)
+        ((= num1 num2) 0)
+        ((< num1 num2) -1)
+    )
+)
 
-(define (make-adder num) 'YOUR-CODE-HERE)
+(define (make-adder num) 
+    (define (f  x) (+ x num))
+    f
+)
 
-(define (composed f g) 'YOUR-CODE-HERE)
+(define (composed f g)
+    (define (newf x)  (f (g x)))newf
+)
 
-(define lst 'YOUR-CODE-HERE)
+(define lst 
+    (list (list 1) 2 (list 3 4 ) 5)
+)
 
-(define (remove item lst) 'YOUR-CODE-HERE)
+(define (remove item lst) 
+    (filter (lambda (x)(not (= x item))) lst)
+)
